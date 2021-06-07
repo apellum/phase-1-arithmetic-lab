3f8fa36e39bf7c704aeaf5286731fcc6953a55ab
Andrew Pellum <apellum19@gmail.com>
2021-06-07 16:17:26 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+const multiply = (num1 * num2);
+
+const random = Math.ceil(Math.random());
+
+const num3 = 4
+const num4 = 30
+
+const mod = (num3 % num4);
+
+const max = Math.max(1, 2, 4, 20);
